1d15b4fba208f69b49809fc31306808a58f86881
OwenFeik <owenhfeik@gmail.com>
2020-03-22 05:44:43 +1100

    update commit script

--- commit.sh
+++ commit.sh
@@ -19,11 +19,11 @@ prev_lec_no=$(awk "/$subj/ {print \$2}" lecture.txt)
 this_lec_no=$((prev_lec_no+1))
 
 sed -i "s/$subj $prev_lec_no/$subj $this_lec_no/" lecture.txt 
-git add lecture.txt
+git add "lecture.txt"
 
 commit_msg="$subj lecture $this_lec_no"
 
-git commit -m $commit_msg
+git commit -m "$commit_msg"
 git push
 
 if [ ! -z "$repeat" ]; then
