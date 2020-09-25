5d764d0e19b534befa2de8ec4fc52d722fe2a29c
OwenFeik <owenhfeik@gmail.com>
2020-09-25 15:01:49 +1000

    update script to include a PDF

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-subj=$(git status | awk '/notes.tex/ {print $2}') # Get changed note document
-len=${#subj}
+changes=$(git status | awk '/notes.tex/ {print $2}') # Get changed note document
+len=${#changes}
 
 if [ $len -ge 20 ]; then
     read -p '2 or more notes. Commit all (y/n)? ' inp
@@ -13,9 +13,21 @@ elif [ $len -eq 0 ]; then
     exit 0
 fi
 
-subj=$(echo $subj | cut -c -19) # Trim to file name (SUBJ10001/notes.tex)
-git add $subj
-subj=$(echo $subj | cut -c -9) # Trim to subject name (SUBJ10001)
+file=$(echo $changes | cut -c -19) # Trim to file name (SUBJ10001/notes.tex)
+subj=$(echo $file | cut -c -9) # Trim to subject name (SUBJ10001)
+
+pdflatex -shell-escape -synctex=1 -interaction=nonstopmode \
+    -file-line-error -output-directory $subj $file > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Ran into error compiling $file. Exiting."
+    exit 1
+fi
+
+cp "$subj/notes.pdf" "_pdfs/$subj.pdf"
+git add $file
+git add "_pdfs/$subj.pdf"
+
 prev_lec_no=$(awk "/$subj/ {print \$2}" lecture.txt)
 this_lec_no=$((prev_lec_no+1))
 
